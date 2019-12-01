d9e22f80d7e9d172d83ca9580a9f7f3ecf450ba2
fpiskur <filip.piskur@gmail.com>
2019-12-01 22:13:38 +0100

    Revert "Updates"
    This reverts commit 370bc54feebc32824d2aca06d28c99703f11b398.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6253c59ca550d4f08b1306ad4e3ff9d4.js"
+  "/transformers/precache-manifest.3d99f404ed905cc4e3a602e28d866731.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/transformers/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
